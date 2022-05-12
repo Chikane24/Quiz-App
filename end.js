866d0fa82186c06566cdfc2f3e95b0d3c187f4ab
Chikane24 <amitachikane9@gmail.com>
2022-05-12 14:54:31 +0530

    Saved top 5 highscores in local storage

--- end.js
+++ end.js
@@ -5,10 +5,26 @@ const saveUser = document.getElementById("saveScoreBtn")
 let recentScore = localStorage.getItem("recentScore");
 finalScore.innerText = recentScore;
 
+const highScores = JSON.parse(localStorage.getItem("highScore")) || [];
+console.log(highScores);
+const MAX_HIGH_SCORE = 5
+
 userName.addEventListener("keyup", () => {
     saveUser.disabled = !userName.value;
 });
 
 saveHighScore = e =>{
     e.preventDefault();
+
+    const score = {
+        score: recentScore,
+        name: userName.value
+    };
+    highScores.push(score);
+    highScores.sort((a,b)=> b.score-a.score);
+    highScores.splice(5);
+    console.log(highScores);
+
+    localStorage.setItem("highScores",JSON.stringify(highScores));
+    
 }
